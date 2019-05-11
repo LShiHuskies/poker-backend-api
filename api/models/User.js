6c62ba843fd82c65a44805c5e613abd3727bd045
Louis Shi <Shilou2010@gmail.com>
2019-05-11 17:22:33 -0400

    session auth

--- api/models/User.js
+++ api/models/User.js
@@ -4,6 +4,7 @@
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
  */
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
@@ -25,7 +26,20 @@ module.exports = {
     firstName: { type: 'string', required: true },
     lastName: { type: 'string', required: true },
     userName: { type: 'string', required: true },
-    password: { type: 'string' },
+    password: {
+      type: 'string',
+      minLength: 6,
+      protected: true,
+      required: true,
+      columnName: "encryptedPassword"
+    },
+    confirmation: {
+      type: 'string',
+      minLength: 6,
+      protected: true,
+      required: true,
+      columnName: "encryptedPassword"
+    },
     admin: { type: 'boolean', defaultsTo: false },
     email: {
       type: 'string',
@@ -47,6 +61,33 @@ module.exports = {
       type: 'number'
     }
   },
+  toJSON: function() {
+    let obj = this.toObject();
+    delete obj.password;
+    delete obj.confirmation;
+  },
+  beforeCreate: function(values, cb) {
+    bcrypt.hash(values.password, 10, function (err, hash) {
+      if (err) return cb(err);
+
+      values.password = hash;
+      cb();
+    });
+  },
+  comparePassword: function(password, user) {
+    return new Promise(function (resolve, reject) {
+      bcrypt.compare(password, user.password, function (err, match) {
+        if (err) reject(err);
+
+        if (match) {
+          resolve(true);
+        } else {
+          reject(err);
+        }
+      })
+    });
+  }
+
 
 
 };
