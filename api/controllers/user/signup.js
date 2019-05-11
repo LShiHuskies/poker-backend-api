ce0b03f998a7f1057b84fdee814743a3ad11dab0
Louis Shi <Shilou2010@gmail.com>
2019-05-11 15:16:00 -0400

    second commit

---
+++ api/controllers/user/signup.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Signup',
+
+
+  description: 'Signup user.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs, exits) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
---
+++ api/models/User.js
@@ -0,0 +1,44 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+    firstName: { type: 'string', required: true },
+    lastName: { type: 'string', required: true },
+    userName: { type: 'string', required: true },
+    admin: { type: 'boolean', defaultsTo: false },
+    email: {
+      type: 'string',
+      isEmail: true,
+      required: true,
+      unique: true,
+    },
+    encryptedPassword: {
+      type: 'string'
+    },
+  },
+
+  beforeCreate: function (values, next) {
+    
+  },
+
+};
